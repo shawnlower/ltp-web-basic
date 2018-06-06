dee5fac9b7be3797a0adc410e209294e88b8e248
Shawn Lower <shawn@shawnlower.com>
2018-06-05 23:14:16 -0400

    update modal template, add key events for closing modal

--- src/app/components/basic-editor/basic-editor.component.ts
+++ src/app/components/basic-editor/basic-editor.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  HostListener,
   Input,
   OnInit
 } from '@angular/core';
@@ -9,6 +10,9 @@ import { FormArray, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Action, Store } from '@ngrx/store';
 
 import { Subject, Observable } from 'rxjs';
+
+import * as fromRoot from '../../reducers';
+import * as appActions from '../../actions/app.actions';
 
 import { Item } from '../../models/item.model';
 
@@ -20,30 +24,39 @@ interface AppState {
 @Component({
   selector: 'app-basic-editor',
   templateUrl: './basic-editor.component.html',
-  styleUrls: ['./basic-editor.component.css']
+  styleUrls: ['./basic-editor.component.css'],
 })
 export class BasicEditorComponent implements OnInit {
 
   @Input() item: Item;
+  @Input() typeUrl: Item;
   @Input() testitem: Item;
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      // close modal
+      this.store.dispatch(new appActions.ToggleEditor());
+    }
+  }
+
+  constructor(private formBuilder: FormBuilder,
+              public store: Store<fromRoot.State>) {
     this.form = this.formBuilder.group({
-      url: 'default url',
+      typeUrl: '',
       dataType: 'default dataType'
     });
 
+    this.form.controls['typeUrl'].valueChanges.subscribe(v => console.log('typeUrl', v));
   }
 
   onChanges(): void {
-    this.form.valueChanges.subscribe(val => {
-      console.log(val);
-    });
+    console.log('this', this);
   }
 
   ngOnInit() {
-      console.log(this.item);
   }
 
 }
