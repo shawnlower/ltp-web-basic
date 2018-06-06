f8977ab4646ee0dc843a0950ad65d036f675fe27
Shawn Lower <shawn@shawnlower.com>
2018-06-05 21:05:24 -0400

    Ignore key events when modal or searchbox focused

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -20,14 +20,29 @@ export class AppComponent {
   @ViewChildren('searchBox') vc;
 
   showEditor$: Observable<boolean>;
+  showEditor: boolean;
 
   constructor(public store: Store<fromRoot.State>) {
-    this.showEditor$ = store.select(state => state.app.showEditor);
+    // store.select(state => this.showEditor = state.app.showEditor);
+
+    this.showEditor$ = store.select(state => {
+      this.showEditor = state.app.showEditor;
+      return state.app.showEditor;
+    });
   }
 
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
-    console.log(event);
+    // Handle keyboard events
+
+
+    // ignore inputs if editor modal is shown or search focused
+    const searchFocused =
+      document.activeElement === document.getElementsByName('search')[0];
+
+    if (this.showEditor || searchFocused) {
+      return;
+    }
 
     if ( event.key === '/' ) {
       this.vc.first.nativeElement.focus();
@@ -41,11 +56,12 @@ export class AppComponent {
       this.store.dispatch(new appActions.SelectPrevItem());
     } else if ( event.key === 'e' ) {
       // Toggle the editor
-      console.log(this.showEditor$.pipe(v => v));
       this.toggleEditor();
     } else if ( event.key === 'n' ) {
       // Toggle the editor
       this.toggleEditor();
+    } else {
+      console.log('keyEvent: no handler for key: ', event.key);
     }
   }
 
