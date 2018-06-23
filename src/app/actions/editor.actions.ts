a7e7d33b8456f97ba304a83cc443da649615d97b
Shawn Lower <shawn@shawnlower.com>
2018-06-22 23:22:00 -0400

    Add new action/reducer for opened editor

--- src/app/actions/editor.actions.ts
+++ src/app/actions/editor.actions.ts
@@ -5,8 +5,9 @@ import { Item } from '../models/item.model';
 export const LOAD_ITEM = '[Action] Load Item';
 export const RESET_EDITOR = '[Action] Reset Editor';
 export const EDITOR_CLOSED = '[Action] Editor Closed';
+export const EDITOR_OPENED = '[Action] Editor Opened';
 
-/**
+/*
  * Reset all controls in editor modal
  */
 export class ResetEditor implements Action {
@@ -16,8 +17,23 @@ export class ResetEditor implements Action {
   }
 }
 
+/*
+ * Indicates that the editor modal was just closed
+ */
 export class EditorClosed implements Action {
   readonly type = EDITOR_CLOSED;
+
+  constructor() {
+  }
+}
+
+/*
+ * Indicates that the editor modal was just opened
+ *  - Load item if needed
+ *  - Load default item
+ */
+export class EditorOpened implements Action {
+  readonly type = EDITOR_OPENED;
 
   constructor() {
   }
@@ -33,5 +49,7 @@ export class LoadItem implements Action {
 
 export type Actions
   = ResetEditor
-  | ResetEditor;
+  | EditorOpened
+  | EditorClosed
+  | LoadItem;
 
