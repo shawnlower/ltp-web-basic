8c1365f228d6f4d921030d8e1374bfa44d3b508a
Shawn Lower <shawn@shawnlower.com>
2018-06-30 23:34:18 -0400

    disable debug on workbox

--- src/sw.js
+++ src/sw.js
@@ -3,9 +3,9 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 if (workbox) {
   console.log('Workbox is loaded 🎉');
 
-  workbox.LOG_LEVEL = 'debug';
+//  workbox.LOG_LEVEL = 'debug';
   workbox.setConfig({
-    debug: true
+    debug: false
   });
 
   /*
